be9415ca9577b74d6d9610ce5c527022ecae4467
Ilia Pikulev <cobanja1@gmail.com>
2024-02-10 16:18:44 +1300

    Adjusting to GH pages

--- build/_app/immutable/entry/start.3R49TcOD.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"../chunks/entry.5mt5zocR.js";export{a as start};
--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    appDir: 'app',
+    paths: {
+      base: '/quinque'
+    },
     adapter: adapter({
       fallback: 'index.html'
     }),
