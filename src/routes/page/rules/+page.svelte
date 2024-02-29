64fbb6963f7cffb6c1ef26c4827aaceb6bcc0e7e
Ilia Pikulev <cobanja1@gmail.com>
2024-02-29 22:06:45 +1300

    Rules minor rewording

--- src/routes/page/rules/+page.svelte
+++ src/routes/page/rules/+page.svelte
@@ -6,7 +6,7 @@
       <li class={liClasses}>You and your opponent have to fill the grid with your colour;</li>
       <li class={liClasses}>Set your colour on the grid by tapping the square without a colour;</li>
       <li class={liClasses}>The game takes places in turns: First player, then Second player, then First player again and so on;</li>
-      <li class={liClasses}>Each turn has got a pattern the blocks will be filled on the grid;</li>
+      <li class={liClasses}>Each turn follows a specific pattern for filling blocks on the grid;</li>
       <li class={liClasses}>
         The patterns are:
         <ul>
@@ -20,15 +20,15 @@
           <li class={liClasses}><Icon name={customIconVertical} class={iconClasses} />&nbsp;&nbsp;5 blocks selection in vertical axis;</li>
         </ul>
       </li>
-      <li class={liClasses}>Single block selection always starts first;</li>
+      <li class={liClasses}>Single block selection always starts the game;</li>
       <!-- eslint-disable-next-line max-len -->
       <li class={liClasses}>Each turn (except the very first one) receives the pattern randomly, but players always know the current and the next two patterns;</li>
       <!-- eslint-disable-next-line max-len -->
-      <li class={liClasses}>Each colored block on the grid gives the player 5 points. However, if the block is adjacent to a block with the same colour, one point is deducted from that score (once per axis). So, two adjacent blocks will give 8 instead of 10 (4 + 4), and three adjacent blocks horizontally and vertically wil give 9 instead of 15. So be mindful of where you place your blocks.</li>
+      <li class={liClasses}>Each colored block on the grid gives the player 5 points. However, if the block is adjacent to a block with the same colour, one point is deducted from that score (once per axis). So, two adjacent blocks will give 8 instead of 10 (4 + 4), and three adjacent blocks horizontally and vertically will give 9 instead of 15. So be mindful of where you place your blocks.</li>
       <li class={liClasses}>Once the block is in place, it becomes inactive and it stores the score value for the given colour;</li>
       <!-- eslint-disable-next-line max-len -->
-      <li class={liClasses}>When a coloured block is placed, it can flip the colour of any adjacent block(s) if it jams a separate colored block between itself and the old block of the same colour.</li>
-      <li class={liClasses}>The game ends when there are no free blocks are left on the grid;</li>
+      <li class={liClasses}>When a colored block is placed, it can change the color of adjacent blocks if it's positioned next to a block of the same color on both sides, effectively 'flipping' them.</li>
+      <li class={liClasses}>The game ends when there are no free blocks left on the grid;</li>
       <li class={liClasses}>The player with the highest number of points wins.</li>
       <li class={liClasses}>Enjoy the game and good luck outsmarting the opponent!</li>
     </ul>
