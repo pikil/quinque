f8865d2fdc171ef4ad71485c930b236558578c83
Ilia Pikulev <cobanja1@gmail.com>
2024-04-06 10:59:35 +1300

    Removing ssl for now

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import { https } from './dev_srv.json'
+// import { https } from './dev_srv.json'
 
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    proxy: {},
-    https
+    proxy: {}
+    // https // Use this for setting up ssl
   }
 })
