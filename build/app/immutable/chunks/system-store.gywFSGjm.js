d912602c989bb5b8498ebff206b2ea913e179333
Ilia Pikulev <cobanja1@gmail.com>
2024-04-30 23:35:43 +1200

    Version 1.1.5 packaged

---
+++ build/app/immutable/chunks/system-store.gywFSGjm.js
@@ -0,0 +1,1 @@
+import{n as o}from"./scheduler.Btf-vRGu.js";import{w as s}from"./index.NjmGTWo3.js";const t=s({text:"",ok:o,okLabel:"OK",dismissLabel:"Cancel",dismiss:o});export{t as c};
---
+++ build/app/immutable/entry/start.BT3VHHef.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BglvYtTI.js";export{t as start};
