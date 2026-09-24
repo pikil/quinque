cde3a9fcdf3a72fe79511069b60f8e7d7ede185e
Ilia Pikulev <cobanja1@gmail.com>
2026-09-24 19:39:32 +1200

    Updating vite and tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import { https } from './dev_srv.json'
+import { https } from './dev_srv.json' with { type: 'json' }
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit()
+  ],
   server: {
     https, // Use this for using SSL (essential for online mode)
     proxy: {}
